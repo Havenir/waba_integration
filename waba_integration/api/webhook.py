3ac1a4f64d69f1ac5df2c3cd9dae52261d8e87d2
Ahmad <ahmad@havenir.com>
2024-11-13 22:16:59 +0500

    fix: log webhook error

--- waba_integration/api/webhook.py
+++ waba_integration/api/webhook.py
@@ -1,7 +1,7 @@
 import frappe
 from waba_integration.whatsapp_business_api_integration.doctype.waba_whatsapp_message.waba_whatsapp_message import (
-	create_waba_whatsapp_message,
-	process_status_update,
+    create_waba_whatsapp_message,
+    process_status_update,
 )
 
 from werkzeug.wrappers import Response
@@ -9,33 +9,37 @@ from werkzeug.wrappers import Response
 
 @frappe.whitelist(allow_guest=True)
 def handle():
-	if frappe.request.method == "GET":
-		return verify_token_and_fulfill_challenge()
+    if frappe.request.method == "GET":
+        return verify_token_and_fulfill_challenge()
 
-	try:
-		form_dict = frappe.local.form_dict
-		messages = form_dict["entry"][0]["changes"][0]["value"].get("messages", [])
-		statuses = form_dict["entry"][0]["changes"][0]["value"].get("statuses", [])
+    try:
+        form_dict = frappe.local.form_dict
+        messages = form_dict["entry"][0]["changes"][0]["value"].get("messages", [])
+        statuses = form_dict["entry"][0]["changes"][0]["value"].get("statuses", [])
 
-		for status in statuses:
-			process_status_update(status)
+        for status in statuses:
+            process_status_update(status)
 
-		for message in messages:
-			create_waba_whatsapp_message(message)
+        for message in messages:
+            create_waba_whatsapp_message(message)
 
-		frappe.get_doc(
-			{"doctype": "WABA Webhook Log", "payload": frappe.as_json(form_dict)}
-		).insert(ignore_permissions=True)
-	except Exception:
-		frappe.log_error("WABA Webhook Log Error", frappe.get_traceback())
-		frappe.throw("Something went wrong")
+        frappe.get_doc(
+            {"doctype": "WABA Webhook Log", "payload": frappe.as_json(form_dict)}
+        ).insert(ignore_permissions=True)
+    except Exception:
+        form_dict = frappe.local.form_dict
+        frappe.get_doc(
+            {"doctype": "WABA Webhook Log", "payload": frappe.as_json(form_dict)}
+        ).insert(ignore_permissions=True)
+
+        frappe.log_error("WABA Webhook Log Error", frappe.get_traceback())
 
 
 def verify_token_and_fulfill_challenge():
-	meta_challenge = frappe.form_dict.get("hub.challenge")
-	expected_token = frappe.db.get_single_value("WABA Settings", "webhook_verify_token")
+    meta_challenge = frappe.form_dict.get("hub.challenge")
+    expected_token = frappe.db.get_single_value("WABA Settings", "webhook_verify_token")
 
-	if frappe.form_dict.get("hub.verify_token") != expected_token:
-		frappe.throw("Verify token does not match")
+    if frappe.form_dict.get("hub.verify_token") != expected_token:
+        frappe.throw("Verify token does not match")
 
-	return Response(meta_challenge, status=200)
+    return Response(meta_challenge, status=200)
